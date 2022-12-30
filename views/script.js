5b80cb3c6d49871dd79c4e41b7a31e80a4365a51
MaxenceLearn <maxence.rabrais@gmail.com>
2022-12-31 00:25:09 +0100

    Changes

--- views/script.js
+++ views/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.button').forEach(item => {    
+document.querySelectorAll('.button').forEach(item => {
     item.addEventListener('click', event => {
         document.querySelectorAll('.button').forEach(item => {
             item.style.filter = 'invert(0%)'
@@ -7,10 +7,10 @@ document.querySelectorAll('.button').forEach(item => {
         document.querySelector('.selector').style.left = `${document.getElementById(`${event.target.id}`).getBoundingClientRect().left - 23.5}px`
         document.getElementById(`${event.target.id}`).style.filter = 'invert(100%)'
     })
-  })
+})
 
 
-  document.querySelectorAll('.topic').forEach(item => {    
+document.querySelectorAll('.topic').forEach(item => {
     item.addEventListener('click', event => {
         document.querySelectorAll('.topic').forEach(item => {
             item.style.border = '1px solid rgb(231, 231, 231)'
@@ -21,7 +21,7 @@ document.querySelectorAll('.button').forEach(item => {
         event.target.style.border = '1px solid rgb(0, 0, 0)'
         event.target.id = 'selected'
     })
-  })
+})
 
 
 function getHome() {
@@ -70,34 +70,29 @@ getHome()
 
 
 
-items = document.querySelectorAll(".element");//Get items
-artcontent = document.querySelector(".art-content");//Get art-content
-items.forEach(function(item) {
-  item.addEventListener("click", function() {
-    newelement = document.createElement(`textarea`);//Create new element
-    newelement.setAttribute('draggable', 'true');//Set draggable
-    newelement.setAttribute(`id`, `${item.id}`);//Set classLogo
-    newelement.setAttribute(`class`, `${item.id}`);//Set class
-    newelement.classList.add('to-publish');//Add class to-publish
-    artcontent.appendChild(newelement);
-    toggleMenu();
-  });
-});
-
 
 function RTS() {
     list = []
-    list.push({'type': 'title', 'value': document.querySelector('.edit-title-input').value})
-    list.push({'type': 'description', 'value': document.querySelector('.edit-textarea').value})
-    list.push({'type': 'topic', 'value': document.getElementById('selected').innerText.toLowerCase()})
-    
+    list.push({
+        'type': 'title',
+        'value': document.querySelector('.edit-title-input').value
+    })
+    list.push({
+        'type': 'description',
+        'value': document.querySelector('.edit-textarea').value
+    })
+    list.push({
+        'type': 'topic',
+        'value': document.getElementById('selected').innerText.toLowerCase()
+    })
+
     document.querySelectorAll('.to-publish').forEach(item => {
         value = item.value
         dico = {
             'type': `${item.id}`,
             'value': `${value}`,
         }
-        
+
         list.push(dico)
     })
     console.log(list)
@@ -110,43 +105,42 @@ function imageload() {
     img.src = document.getElementById('toload').value;
     console.log(img.src)
     if (img.complete) {
-      console.log('complete')
-      document.querySelector('.to-publish').style.backgroundImage = `url('${document.getElementById('toload').value}')`
-      document.getElementById('toload').style.border = '1px solid #e7e7e7'
-    } else {
-      img.onload = () => {
-        document.querySelector('.to-publish').style.backgroundImage = `url(${document.getElementById('toload').value})`
+        console.log('complete')
+        document.querySelector('.to-publish').style.backgroundImage = `url('${document.getElementById('toload').value}')`
         document.getElementById('toload').style.border = '1px solid #e7e7e7'
-      };
-      img.onerror = () => {
-        console.log('error')
-        document.getElementById('toload').style.border = '1px solid red'
-      };
+    } else {
+        img.onload = () => {
+            document.querySelector('.to-publish').style.backgroundImage = `url(${document.getElementById('toload').value})`
+            document.getElementById('toload').style.border = '1px solid #e7e7e7'
+        };
+        img.onerror = () => {
+            console.log('error')
+            document.getElementById('toload').style.border = '1px solid red'
+        };
     }
 }
 
 document.getElementById('signup').addEventListener('click', () => {
-    fetch('/log',
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+    fetch('/log', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 'pseudo': document.getElementById('pseudo').value,
                 'password': document.getElementById('password').value,
                 'avatar': document.getElementById('avatar').value
             })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            alert('Some informations are missing not unique or incorrect')
-        } else {
-            console.log(data)
-            document.getElementById('signup-popup').style.display = 'none'
-        }
-    })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                alert('Some informations are missing not unique or incorrect')
+            } else {
+                console.log(data)
+                document.getElementById('signup-popup').style.display = 'none'
+            }
+        })
 })
 
 let log = document.querySelectorAll('.sign')
@@ -164,101 +158,146 @@ log.forEach(item => {
 
 document.getElementById('signin').addEventListener('click', () => {
     console.log('signin')
-    fetch('/log/login',
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+    fetch('/log/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 'pseudo': document.getElementById('pseudo-s').value,
                 'password': document.getElementById('password-s').value
             })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            alert('Some informations are missing not unique or incorrect')
-        } else {
-            document.getElementById('signin-popup').style.display = 'none'
-            document.querySelector('.buttons-log').style.display = 'none'
-            document.querySelector('.user-infos').style.display = 'flex'
-            checkLogin()
-        }
-    })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                alert('Some informations are missing not unique or incorrect')
+            } else {
+                document.getElementById('signin-popup').style.display = 'none'
+                document.querySelector('.buttons-log').style.display = 'none'
+                document.querySelector('.user-infos').style.display = 'flex'
+                checkLogin()
+            }
+        })
 })
 
 function checkLogin() {
     fetch('/getinfos')
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            if (data.logged === false) {
-                console.log('not logged')
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                console.log(data.error)
             } else {
-                document.querySelector('.buttons-log').style.display = 'none'
-                document.querySelector('.user-infos').style.display = 'flex'
-                document.getElementById('user-name').innerHTML = `${data.pseudo}`
-                document.querySelector('.user-img').src = `${data.avatar}`
+                if (data.logged === false) {
+                    console.log('not logged')
+                } else {
+                    document.querySelector('.buttons-log').style.display = 'none'
+                    document.querySelector('.user-infos').style.display = 'flex'
+                    document.getElementById('user-name').innerHTML = `${data.pseudo}`
+                    document.querySelector('.user-img').src = `${data.avatar}`
+                }
             }
-        }
-    })
+        })
 }
 checkLogin()
 
-const container = document.querySelector('.art-content');
 
-// Add the draggable attribute and the dragstart, dragenter, dragover, and dragleave event listeners to the to-publish elements
-container.addEventListener('mousedown', e => {
-  if (e.target.matches('.to-publish')) {
-    const elem = e.target;
-    elem.setAttribute('draggable', true);
-    elem.addEventListener('dragstart', e => {
-      // Set the data that will be transferred during the drag
-      e.dataTransfer.setData('text/plain', null);
+function enableDragSort(listClass) {
+    const sortableLists = document.getElementsByClassName(listClass);
+    Array.prototype.map.call(sortableLists, (list) => {
+        enableDragList(list)
+    });
+}
 
-      // Add the 'dragging' class to the element being dragged
-      // This can be used to apply styles to the element while it is being dragged
-      elem.classList.add('dragging');
+function enableDragList(list) {
+    Array.prototype.map.call(list.children, (item) => {
+        enableDragItem(item)
     });
-    elem.addEventListener('dragenter', e => {
-      // Prevent the default behavior
-      e.preventDefault();
+}
 
-      // Add the 'over' class to the container
-      // This can be used to apply styles to the container while an element is being dragged over it
-      container.classList.add('over');
+function enableDragItem(item) {
+    item.setAttribute('draggable', true)
+    item.ondrag = handleDrag;
+    item.ondragend = handleDrop;
+}
+
+function handleDrag(item) {
+    const selectedItem = item.target,
+        list = selectedItem.parentNode,
+        x = item.clientX;
+    y = item.clientY;
+
+    selectedItem.classList.add('drag-sort-active');
+    let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
+
+    if (list === swapItem.parentNode) {
+        swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+        list.insertBefore(selectedItem, swapItem);
+    }
+}
+
+function handleDrop(item) {
+    item.target.classList.remove('drag-sort-active');
+}
+
+
+items = document.querySelectorAll(".element"); //Get items
+artcontent = document.querySelector(".art-content"); //Get art-content
+items.forEach(function(item) {
+    item.addEventListener("click", function() {
+        switch (item.id) {
+            case 'paragraph':
+                paragraph = document.createElement('li')
+                paragraph.innerHTML = '<textarea id="paragraph" class="to-publish paragraph"></textarea>'
+                paragraph.setAttribute('draggable', 'true')
+                paragraph.setAttribute('class', 'test')
+                artcontent.appendChild(paragraph)
+                break;
+            case 'h1':
+                h1 = document.createElement('li')
+                h1.innerHTML = '<textarea id="h1" class="to-publish h1"></textarea>'
+                h1.setAttribute('draggable', 'true')
+                h1.setAttribute('class', 'test')
+                artcontent.appendChild(h1)
+                break;
+            case 'h2':
+                h2 = document.createElement('li')
+                h2.innerHTML = '<textarea id="h2" class="to-publish h2"></textarea>'
+                h2.setAttribute('draggable', 'true')
+                h2.setAttribute('class', 'test')
+                artcontent.appendChild(h2)
+                break;
+            case 'h3':
+                h3 = document.createElement('li')
+                h3.innerHTML = '<textarea id="h3" class="to-publish h3"></textarea>'
+                h3.setAttribute('draggable', 'true')
+                h3.setAttribute('class', 'test')
+                artcontent.appendChild(h3)
+                break;
+            case 'quote':
+                quote = document.createElement('li')
+                quote.innerHTML = '<textarea id="quote" class="to-publish quote"></textarea>'
+                quote.setAttribute('draggable', 'true')
+                quote.setAttribute('class', 'test')
+                artcontent.appendChild(quote)
+                break;
+            case 'image':
+                image = document.createElement('li')
+                image.innerHTML = '<div class="col-img"><img class="img-td" src="./assets/noimage.png"><div class="row-upload"><input id="image" placeholder="Image url" class="to-publish quote"><button style="margin-right: 0; border-radius: 0; width: 50%;" class="edit-publish">Upload</button></div></div>'
+                image.setAttribute('draggable', 'true')
+                image.classList.add('img-size')
+                image.classList.add('test')
+                artcontent.appendChild(image)
+                break;
+            case 'callout':
+                callout = document.createElement('li')
+                callout.innerHTML = '<div class="loudspeaker"><img class="loudspeaker" src="./assets/loudspeaker.svg"></div><textarea id="callout" class="to-publish callout"></textarea>'
+                callout.setAttribute('draggable', 'true')
+                callout.setAttribute('class', 'test')
+                artcontent.appendChild(callout)
+                break;
+        }
+        enableDragSort('art-content')
+        toggleMenu()
     });
-    elem.addEventListener('dragover', e => {
-      // Prevent the default behavior
-      e.preventDefault();
-    });
-    elem.addEventListener('dragleave', e => {
-      // Remove the 'over' class from the container
-      container.classList.remove('over');
-    });
-  }
 });
-
-// Add the drop event listener to the container
-container.addEventListener('drop', e => {
-  // Prevent the default behavior
-  e.preventDefault();
-
-  // Get the element being dragged
-  const elem = document.querySelector('.dragging');
-
-  // Get the target element (the element under the dragged element)
-  const target = e.target;
-
-  // Exchange the positions of the element being dragged and the target element
-  container.insertBefore(elem, target);
-  container.insertBefore(target, elem);
-
-  // Remove the 'dragging' and 'over' classes
-  elem.classList.remove('dragging');
-  container.classList.remove('over');
-});
-
