a8dd7d248892998052598495d4aa583e37e2cbe3
Your Name <maxence.rabrais@gmail.com>
2023-01-29 22:42:38 +0530

    Changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '2500');
+const port = normalizePort(process.env.PORT || '2501');
 app.set('port', port);
 
 const errorHandler = error => {
